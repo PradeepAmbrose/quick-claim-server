02404dd8733696f78909a251964e4b32bf39be8b
Pradeep <pka@gmail.com>
2023-01-12 13:04:14 +0000

    first commit

---
+++ src/main/java/com/allstate/Quickclaimserver/QuickClaimServerApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.Quickclaimserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickClaimServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickClaimServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allstate/Quickclaimserver/QuickClaimServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstate.Quickclaimserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickClaimServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
